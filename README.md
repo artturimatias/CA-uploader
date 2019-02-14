0aa330e2e74f447f76aa84d960f5300fbb7a2a16
Ari Häyrinen <ari.hayrinen@jyu.fi>
2019-02-14 11:48:27 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # CA-uploader
-A puppeteer based file uploader for CollectiveAccess
+
+
+A Puppeteer based file uploader for [CollectiveAccess](https://collectiveaccess.org/) 
+
+## What is this?
+
+Currently CollectiveAccess has no REST api for uploading files for media representations. This is a workaround for that. USE AT YOUR OWN RISK!
+
+The script uses [Puppeteer](https://github.com/GoogleChrome/puppeteer) for uploading image through the GUI of CollectiveAccess. The user must provide a csv file with image label, file name and object id.
+
+## Install
+
+
+    git clone https://github.com/artturimatias/CA-uploader.git
+    cd CA-uploade
+    npm install
+
+
+## Usage
+First you need to define the address of your CA instance and user credentials. These are defined in file.loader.js.
+
+
+    // CA username
+    const username = "administrator";
+    
+    // CA password
+    const password = "";
+    
+    // URL of your CA instance providence login page
+    const login_url = "http://localhost/providence";
+    
+    // URL of your CA instance object editor where you can add media representations
+    const url = 'http://localhost/providence/index.php/editor/objects/ObjectEditor/Edit/Screen41/object_id/';
+    
+    // path of your local files
+    const path = '/media/sf_downloads/DuoPrev/'
+    
+    // CSV filename
+    const filename = "links.csv"
+
+Project contains a sample csv file (links.csv) wiht sample files. You can test with your TEST ENVIRONMENT by typing:
+    
+        node file-upload
+
+
+This should open browser and you can see how files are uploaded :)
+
+## running headless
+
+If you want to run headless (without seeing the browser), just edit this line:
+
+    const browser = await puppeteer.launch({headless: false});
+to this:
+
+    const browser = await puppeteer.launch({headless: true});
+
---
+++ file-upload.js
@@ -0,0 +1,125 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+var csv = require("fast-csv");
+
+// ************************************************************************
+// CA username
+const username = "administrator";
+
+// CA password
+const password = "";
+
+// URL of your CA instance login page
+const login_url = "http://localhost/providence";
+
+// URL of your CA instance object editor where you can add media representations
+const url = 'http://localhost/providence/index.php/editor/objects/ObjectEditor/Edit/Screen41/object_id/';
+
+// path of your local files
+const path = '/media/sf_downloads/DuoPrev/'
+
+const filename = "links.csv"
+// ************************************************************************
+
+var links = [];
+var stream = fs.createReadStream(filename);
+ 
+var csvStream = csv()
+	.on("data", function(data){
+		 links.push(data);
+	})
+	.on("end", function(){
+		console.log("CSV read!");
+		main(links, function(failures) {
+				console.log("Done");
+			}));
+	});
+ 
+stream.pipe(csvStream);
+
+async function main(data, cb) {
+		
+			
+
+		// open browser
+		const page = await browser.newPage();
+		await page.setViewport({width: 1600, height: 920})
+		await page.goto(login_url);
+
+		// log in
+		await page.type('[name="username"]', username);
+		await page.type('[name="password"]', password);
+		await Promise.all([
+		  page.click('a'),
+		  page.waitForNavigation()
+		]);
+		
+		
+		// for debugging, can be removed
+		await page.setRequestInterception(true);
+		
+		// for debugging, can be removed
+		page.on('response', response => {
+			if (response.url().includes("/object_id")) {
+				  console.log("response code: ", response.status());
+		  }
+		});
+
+		// for debugging, can be removed
+		page.on('request', request => {
+			if (request.method() == "POST") {
+				console.log(request.method() + ':' + request.url());
+			}
+			request.continue();
+		});
+
+
+	// upload images
+	for(const image of data) {
+		if (fs.existsSync(path + image[1])) {
+			if(fs.lstatSync(path + image[1]).isFile()) {
+				try {
+					console.log('Opening: ' + url + image[2]);
+					await page.waitFor(2000);
+					await page.goto(url + image[2],  {waitUntil: 'networkidle0'});
+				   
+					// click "Add representation" link
+					const thumb = await page.$('.caObjectRepresentationListItemImageThumb');
+					if(thumb) {
+							await page.click('.caAddItemButton a');
+							//await page.waitFor(200);
+					}
+					// set label
+					var label = image[0].replace(/"/g,'');
+					const textareas = await page.$$('textarea');
+					await textareas[textareas.length-1].type(label);
+					// set filename
+					const inputs = await page.$$('input[type="file"]');
+					await inputs[inputs.length-1].uploadFile(path + image[1])
+					
+					console.log('Uploading: ' + image[1]);
+				   
+					// for debugging
+					//await page.evaluate(() => {
+						//let dom = document.querySelector('.control-box-left-content a.form-button span');
+						//dom.innerHTML = "Clicking this"
+					//});
+					
+					await Promise.all([
+							page.click('.control-box-left-content a.form-button'),
+							page.waitForNavigation()
+					]);    
+					   
+				} catch(e) {
+						console.log('File upload failed: ' + e.message);
+				}
+			} else {
+					console.log('File not found: ' + image[1]);
+			}
+		} else {
+			console.log("not found: " + path + image[1]));
+		}
+	}
+	cb();
+}
+
