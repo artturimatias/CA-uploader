2cd8d28ae0ed93ae337fd3b23e893887c2eadbb9
Ari Häyrinen <ari.hayrinen@jyu.fi>
2019-02-19 18:43:13 +0200

    try/catch additions

--- uploader.js
+++ uploader.js
@@ -13,7 +13,7 @@ const password = "";
 const login_url = "http://localhost/providence";
 
 // URL of your CA instance object editor where you can add media representations (installation specific)
-const url = 'http://localhost/providence/index.php/editor/objects/ObjectEditor/Edit/Screen42/object_id/';
+const url = 'https://oscari.oscapps.jyu.fi/providence/index.php/editor/objects/ObjectEditor/Edit/Screen41/object_id/';
 
 // path of your local files
 const path = 'files/'
@@ -22,6 +22,8 @@ const filename = "links.csv"
 // ************************************************************************
 
 var links = [];
+var failures = [];
+var counter = 0;
 var stream = fs.createReadStream(filename);
  
 var csvStream = csv()
@@ -30,31 +32,48 @@ var csvStream = csv()
 	})
 	.on("end", function(){
 		console.log("CSV read!");
-		main(links);
+		loop(links);
 	});
  
 stream.pipe(csvStream);
 
+async function loop(links) {
+	var i,j,temparray,chunk = 40;
+	for (i = 0, j = links.length; i<j; i+=chunk) {
+		temparray = links.slice(i,i+chunk);
+		// do whatever
+		await main(temparray);
+	}
+}
+
 async function main(data, cb) {
 	
-	var failures = [];
+	var failure = true;
 		
 	const browser = await puppeteer.launch({headless: false});
-
-	// open browser
 	const page = await browser.newPage();
-	await page.setViewport({width: 1600, height: 920})
-	await page.goto(login_url);
+	
+	// keep trying login 
+	while(failure) {
+		try {
+			// open browser
+			await page.setViewport({width: 1600, height: 920})
+			await page.goto(login_url);
 
-	// log in
-	await page.type('[name="username"]', username);
-	await page.type('[name="password"]', password);
-	await Promise.all([
-	  page.click('a'),
-	  page.waitForNavigation()
-	]);
-	
-	
+			// log in
+			await page.type('[name="username"]', username);
+			await page.type('[name="password"]', password);
+			await Promise.all([
+			  page.click('a'),
+			  page.waitForNavigation()
+			]);
+			failure = false;
+		} catch(e) {
+			console.log(e.message);
+			failure = true;
+		}
+	}
+	/*
 	// for debugging, can be removed
 	await page.setRequestInterception(true);
 	
@@ -72,53 +91,71 @@ async function main(data, cb) {
 		}
 		request.continue();
 	});
-
+*/
 
 	// upload images
 	for(const image of data) {
-		if (fs.existsSync(path + image[1])) {
-			if(fs.lstatSync(path + image[1]).isFile()) {
-				try {
-					console.log('Opening: ' + url + image[2]);
-					await page.waitFor(2000);
-					await page.goto(url + image[2],  {waitUntil: 'networkidle0'});
-				   
-					// click "Add representation" link
-					const thumb = await page.$('.caObjectRepresentationListItemImageThumb');
-					if(thumb) {
-							await page.click('.caAddItemButton a');
-							//await page.waitFor(200);
-					}
-					// set label
-					var label = image[0].replace(/"/g,'');
-					const textareas = await page.$$('.formLabel textarea');
-					await textareas[textareas.length-1].type(label);
-					// set filename
-					const inputs = await page.$$('input[type="file"]');
-					await inputs[inputs.length-1].uploadFile(path + image[1])
-					
-					console.log('Uploading: ' + image[1]);
-				   
-					// for debugging
-					//await page.evaluate(() => {
-						//let dom = document.querySelector('.control-box-left-content a.form-button span');
-						//dom.innerHTML = "Clicking this"
-					//});
-					
-					await Promise.all([
-							page.click('.control-box-left-content a.form-button'),
-							page.waitForNavigation()
-					]);    
+		var filename = image[2];
+		var label = image[1].replace(/"/g,'');
+		var obj_id = image[0];
+		failure = true;
+		if (fs.existsSync(path + filename)) {
+			if(fs.lstatSync(path + filename).isFile() && !isNaN(obj_id)) {
+				while(failure) {
+					try {
+
+						console.log('Opening: ' + url + obj_id);
+						await page.waitFor(2000);
+						await page.goto(url + obj_id,  {waitUntil: 'networkidle0'});
 					   
-				} catch(e) {
-						console.log('File upload failed: ' + e.message);
-						failures.push(image[1]);
+						// click "Add representation" link
+						const thumb = await page.$('.caObjectRepresentationListItemImageThumb');
+						if(thumb) {
+								await page.click('.caAddItemButton a');
+								//await page.waitFor(200);
+						}
+						// set label
+						const textareas = await page.$$('.formLabel textarea');
+						await textareas[textareas.length-1].type(label);
+						// set filename
+						const inputs = await page.$$('input[type="file"]');
+						await inputs[inputs.length-1].uploadFile(path + filename)
+						
+						console.log('Uploading: ' + filename);
+					   
+						// for debugging
+						await page.evaluate(() => {
+							let dom = document.querySelector('.control-box-left-content a.form-button span');
+							dom.innerHTML = "Clicking this"
+						});
+						
+						await Promise.all([
+								page.click('.control-box-left-content a.form-button'),
+								page.waitForNavigation()
+						]);   
+						
+						counter++;
+						console.log('processed: ' + counter);
+						failure = false;
+						await page.waitFor(2000); 
+						   
+					} catch(e) {
+							console.log('File upload failed: ' + e.message);
+							//failures.push(filename);
+							try {
+								await page.reload();
+								failure = true; // try again
+							} catch(e) {
+								failure = false; // reload failed, give up
+								failures.push(filename);
+							}
+					}
 				}
 			} else {
-					console.log('File not found: ' + image[1]);
+					console.log('File not found: ' + filename);
 			}
 		} else {
-			console.log("not found: " + path + image[1]);
+			console.log("not found: " + path + filename);
 		}
 	}
 	console.log("\nDone!\n")
